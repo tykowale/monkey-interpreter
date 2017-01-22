d6ed68053e06bcd0970ae7d869746024818f018e
tykowale <tykowale@gmail.com>
2017-01-22 12:49:40 -0600

    ready for actually parsing!

--- parser/parser.go
+++ parser/parser.go
@@ -7,13 +7,20 @@ import (
 	"monkey/token"
 )
 
-type Parser struct {
-	l *lexer.Lexer
+type (
+	prefixParseFn func() ast.Expression
+	infixParseFn  func(ast.Expression) ast.Expression
+)
 
+type Parser struct {
+	l      *lexer.Lexer
 	errors []string
 
 	curToken  token.Token
 	peekToken token.Token
+
+	prefixParseFns map[token.TokenType]prefixParseFn
+	infixParseFns  map[token.TokenType]infixParseFn
 }
 
 func New(l *lexer.Lexer) *Parser {
@@ -124,4 +131,12 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 
 	p.peekError(t)
 	return false
+}
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
+	p.prefixParseFns[tokenType] = fn
+}
+
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
+	p.infixParseFns[tokenType] = fn
 }
